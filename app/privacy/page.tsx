bd599937be89f03100692686c1aac3f598100091
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2024-01-20 00:45:06 -0500

    add privacy policy

---
+++ app/privacy/page.tsx
@@ -0,0 +1,59 @@
+export default async function Page() {
+  return (
+    <article className="container prose mt-6 lg:prose-xl">
+      <h1>Privacy Policy for gettfully</h1>
+      <p>Effective Date: January 20, 2024</p>
+      <p>
+        This Privacy Policy applies to the API developed for searching Getty
+        ULAN and AAT vocabularies, henceforth referred to as
+        &quot;gettfully&quot;. This policy outlines our handling practices for
+        the information we collect and you provide while using our API.
+      </p>
+      <p>1. Data Collection and Use</p>
+      <p>
+        gettfully does not collect personal information from its users. Our
+        service is designed to provide search functionality without the need to
+        store personal data. The API hosted on Vercel may use non-identifying
+        cookies for analytics purposes. These cookies do not collect personal
+        information and are used solely to enhance the performance and
+        functionality of our service.
+      </p>
+      <p>2. No Third-Party Data Sharing</p>
+      <p>
+        We do not share any user data with third parties. Our commitment is to
+        maintain the confidentiality and integrity of your data.
+      </p>
+      <p>3. User Consent</p>
+      <p>
+        By using gettfully, you consent to the collection and use of information
+        as outlined in this Privacy Policy. Since we do not collect personal
+        data, explicit consent for data processing is not required.
+      </p>
+      <p>4. Data Security</p>
+      <p>
+        No user data is stored in our system, thus eliminating risks associated
+        with data storage and security. We prioritize the protection of any data
+        that passes through our API and implement industry-standard measures to
+        safeguard our service.
+      </p>
+      <p>5. User Rights</p>
+      <p>
+        Since gettfully does not collect or store personal data, issues related
+        to access, correction, or deletion of personal data are not applicable.
+      </p>
+      <p>6. Policy Updates</p>
+      <p>
+        We reserve the right to update this Privacy Policy at any time. Changes
+        will be effective immediately upon posting the revised policy. Users are
+        encouraged to periodically review this policy to stay informed about our
+        privacy practices.
+      </p>
+      <p>7. Contact Information</p>
+      <p>
+        If you have any questions or concerns regarding this Privacy Policy or
+        the practices of gettfully, please contact us via GitHub (user:
+        derekphilipau).
+      </p>
+    </article>
+  );
+}
